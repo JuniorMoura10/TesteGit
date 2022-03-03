cd2ce60afa42b04b577b152d5e6b58454c14a80e
JuniorMoura10 <geraldomartins10@gmail.com>
2022-03-03 16:18:46 -0300

    first commit

---
+++ repoGit/assets/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+
+const App: () => Node = () => {
+  return (
+    
+  );
+};
+
+
+
+export default App;
